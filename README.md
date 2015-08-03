b46ee9f50a9a9d7a861ed50117332e4550c3de06
lorentzenman <matt@lorentzen.co.uk>
2015-08-03 10:31:47 +0100

    Added in '--all_issues' option to populate everything in the nessus file into db

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # parsenal
-Parsing tool for extracting data from XML based tool output like Nessus and Nipper
+Parsing tool for extracting data from XML based tool output like Nessus.
+
+parsenal -h
+
+
+[::] Nessus
+Using the sqlite database it is possible to run queries and output the results to text files for report inclusion. If all issues are specified then this will grab all plugins and put them into the database. This gives maximum flexibility in query the data held within the nessus file(s).
+
+When using text output, the tool uses the hardcoded list in the script and it will only grab the output from these plugins.
+
+[::] John
+Using this option will take a john log and then write out the username and password together with the timecracked.
+
+
+[*] Nessus
+
+Usage Examples
+
+parsenal --output db --dbname parsed.db --all_issues nessus /path/to/file.nessus
+
+:: Note that when using '--output db' the database gets appended so you can parse a number of files in a loop
+
+for nessus in $(ls | grep .nessus);do parsenal --output db --dbname parsed --all_issues nessus $nessus;done
+
+
+SQLite Example Queries
+
+:: Set output type and path
+.output /root/output.txt
+
+:: Plugin Output
+select host_name, host_port, plugin_output from issues where plugin_name = "SSL Self-Signed Certificates";
+
+:: Reset back to stdout
+.output stdout
+
+:: Based on Severity - Grabs MEDIUM rating and above
+select host_name, plugin_name from issues where severity > 2;
+
+:: List all plugin_names found
+select distinct plugin_name from issues;
+
+
+[*] John
+
+Usage Examples
+
+parsenal --csvfile /path/to/csv.file john /path/to/john.log
+
+
+
--- parsenal.py
+++ parsenal.py
@@ -3,6 +3,8 @@
 Author : Matt Lorentzen
 Original Date: September 2014
 """
+# version 0.4
+
 
 from xml.dom import minidom
 import sqlite3
@@ -23,10 +25,11 @@ def banner():
 | |_) | (_| | |  \__ \  __/ | | | (_| | |
 | .__/ \__,_|_|  |___/\___|_| |_|\__,_|_|
 |_|
-
+                                           ^
+------------------------------------------ |
 """
 
-	print banner
+	print yellowtxt(banner)
 
 
 
@@ -79,39 +82,65 @@ def setupDB(db):
 				host_name VARCHAR(255),
 				host_port INTEGER,
 				plugin_name VARCHAR(255),
-				plugin_output TEXT
+				plugin_output TEXT,
+				severity INTEGER
 				)
 	""")   
 
 
 
-def parse_nessus_file_db(nessus_doc, checkfor, db):
+def parse_nessus_file_db(nessus_doc, checkfor, db, all_issues, infoplugins):
 	""" Parses nessus file and outputs to sqlite database """
 #	issuefile = open(issuefile, 'r')
 #	checkfor = issuefile.readlines()
 #	issuefile.close()
+
 	for reportHost in nessus_doc.getElementsByTagName("ReportHost"):
 		# start individual host parse
 		host_name = reportHost.attributes["name"].value
 		for reportItem in reportHost.getElementsByTagName("ReportItem"):
 			plugin_name =  reportItem.attributes['pluginName'].value
 			host_port = reportItem.attributes['port'].value
-			# if this is something to check for in the list
-			if plugin_name in checkfor:
-				print greentxt("[::] Found: ") + host_name + " : " + redtxt(host_port) + " :: " + plugin_name
-				# write to database
-				plugin_output = reportItem.getElementsByTagName("plugin_output")
-				# adds a check for the plugin output being empty ie equal to 0
-				# in the db case this needs execute a db_query based on the check
-				if len(plugin_output) > 0:
-					plugin_output = plugin_output[0].firstChild.nodeValue
-					# add this plugin output to the relevant table, formatting of output is kept when populating the table which is cool
-					db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name, plugin_output) VALUES(?,?,?,?,?)", (host_name, host_name, host_port, plugin_name, plugin_output))
-				else:
-					# execute the query without the plugin_output
-					db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name) VALUES(?,?,?,?)", (host_name, host_name, host_port, plugin_name))
+			severity_rating = reportItem.attributes['severity'].value
+			# new checks to show if all criticals are to be grabbed
+			if all_issues == True:
+				if severity_rating > 0 or plugin_name in infoplugins:
+					# convert string to int and check is this is MEDIUM or higher
+					print "The plugin is : " + redtxt(plugin_name) + " >> severity is " + yellowtxt(severity_rating) + " :: " + host_name
+					# write to database
+					plugin_output = reportItem.getElementsByTagName("plugin_output")
+	
+					# adds a check for the plugin output being empty ie equal to 0
+					# in the db case this needs execute a db_query based on the check
+					if len(plugin_output) > 0:
+						plugin_output = plugin_output[0].firstChild.nodeValue
 
-				db.commit()
+						# add this plugin output to the relevant table, formatting of output is kept when populating the table which is cool
+						db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name, plugin_output, severity) VALUES(?,?,?,?,?,?)", (host_name, host_name, host_port, plugin_name, plugin_output, int(severity_rating)))
+					else:
+
+						# execute the query without the plugin_output
+						db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name, severity) VALUES(?,?,?,?,?)", (host_name, host_name, host_port, plugin_name, int(severity_rating)))
+
+					db.commit()
+
+			else:
+					# if this is something to check for in the hard coded list
+					if plugin_name in checkfor:
+						print greentxt("[::] Found: ") + host_name + " : " + redtxt(host_port) + " :: " + plugin_name + yellowtxt(" > "+  severity_rating)
+						# write to database
+						plugin_output = reportItem.getElementsByTagName("plugin_output")
+						# adds a check for the plugin output being empty ie equal to 0
+						# in the db case this needs execute a db_query based on the check
+						if len(plugin_output) > 0:
+							plugin_output = plugin_output[0].firstChild.nodeValue
+							# add this plugin output to the relevant table, formatting of output is kept when populating the table which is cool
+							db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name, plugin_output) VALUES(?,?,?,?,?)", (host_name, host_name, host_port, plugin_name, plugin_output))
+						else:
+							# execute the query without the plugin_output
+							db.execute("INSERT INTO issues(host_ipaddress, host_name, host_port, plugin_name) VALUES(?,?,?,?)", (host_name, host_name, host_port, plugin_name))
+
+						db.commit()
 
 
 
@@ -174,6 +203,48 @@ def bluetxt(text2colour):
 	return bluestart + text2colour + blueend
 
 
+
+def info_plugins():
+	# Informational Plugins with 'severity = 0' within nessus XML file
+	info_plugins = [
+	"Patch Report",
+	"OS Identification",
+	"Microsoft Windows Summary of Missing Patches",
+	"Patch Report"
+	]
+
+	return info_plugins
+
+
+
+def check_for():
+	# Nessus Plugin Names to check for that have severity of informational
+	checkfor = [
+	"MS08-067: Microsoft Windows Server Service Crafted RPC Request Handling Unspecified Remote Code Execution (958644)",
+	"FTP Supports Clear Text Authentication",
+	"Microsoft Windows SMB NULL Session Authentication",
+	"SSL Self-Signed Certificate",
+	"SSL RC4 Cipher Suites Supported",
+	"SSLv2",
+	"OS Identification",
+	"Microsoft Windows Summary of Missing Patches",
+	"SSLv3 Padding Oracle On Downgraded Legacy Encryption Vulnerability (POODLE)",
+	"Terminal Services Doesn't Use Network Level Authentication (NLA) Only",
+	"SSL Version 2 and 3 Protocol Detection",
+	"HP Data Protector 8.x Arbitrary Command Execution (HPSBMU03072)",
+	"HP Data Protector 'EXEC_INTEGUTIL' Arbitrary Command Execution",
+	"MS14-066: Vulnerability in Schannel Could Allow Remote Code Execution (2992611) (uncredentialed check) ",
+	"MS15-034: Vulnerability in HTTP.sys Could Allow Remote Code Execution (3042553) (uncredentialed check)",
+	"VMware vCenter Server Multiple Java Vulnerabilities (VMSA-2015-0003) (POODLE)",
+	"SNMP Agent Default Community Name (public)",
+	"Oracle TNS Listener Remote Poisoning",
+	"NTP monlist Command Enabled",
+	"NFS Share User Mountable"
+	]
+	
+	return checkfor
+
+
 #######################################################################
 #	Main Function   
 #######################################################################
@@ -190,18 +261,58 @@ def main():
 	nessus_parse_group = parser.add_argument_group('Nessus', 'Nessus File Parsing Options')
 
 	nessus_parse_group.add_argument("-o", "--output", help="Specify the output type : <txt> <db>")
-	nessus_parse_group.add_argument("-d", "--dbname", help="Specify the name of the sqlite3 database")
-
+	nessus_parse_group.add_argument("-d", "--dbname", help="Specify the name of the SQLite3 database\n")
+	nessus_parse_group.add_argument("-a", "--all_issues", action="store_true", default=False, help="Using '-a' or '--all_issues' will grab all the ISSUE output into the SQLite3 database")
+	
 	# John Parse Group
 	john_parse_group = parser.add_argument_group('John', 'John File Parsing Options')
 	john_parse_group.add_argument("--csvfile", help="Path to CSV File Output for logfile")
 
 	args = parser.parse_args()
-	print args
+#	print args
 	
 	if args.mode == "nessus":
-		
-		# Nessus Plugin Names to check for
+				
+	
+		print "The file to parse is from Nessus"
+		nessus_doc = minidom.parse(args.inputfile)
+
+		# checking lists	
+		checkfor = check_for()
+		infoplugins = info_plugins()
+
+	
+	#	issuefile = args.issue
+		if args.output == "txt":
+			parse_nessus_file_txt(nessus_doc, checkfor)
+
+		else:
+
+			db_name = args.dbname
+			# create a connection to the database
+			db = sqlite3.connect(db_name)
+			# setup database structure
+			setupDB(db)
+			all_issues = args.all_issues
+			parse_nessus_file_db(nessus_doc, checkfor, db, all_issues, infoplugins)
+	
+	elif args.mode == "nipper":
+		print "The file to parse is Nessus"
+	elif args.mode == "john":
+		print "The file to parse is a John the Ripper Log File"
+		parse_john_log(args.inputfile, args.csvfile)
+	else:
+		print "Invalid File Type"
+
+
+	hrline = "-------------------------------------------------------------------------------------\n"
+
+	
+if __name__ ==  "__main__":
+	main()
+
+
+"""
 		checkfor = [
 		"Microsoft Windows Remote Desktop Protocol Server Man-in-the-Middle Weakness",
 		"Patch Report", 
@@ -228,31 +339,5 @@ def main():
 		
 		]
 	
+"""
 
-		print "The file to parse is from Nessus"
-		nessus_doc = minidom.parse(args.inputfile)
-	#	issuefile = args.issue
-		if args.output == "txt":
-			parse_nessus_file_txt(nessus_doc, checkfor)
-		else:
-			db_name = args.dbname
-			# create a connection to the database
-			db = sqlite3.connect(db_name)
-			# setup database structure
-			setupDB(db)
-			parse_nessus_file_db(nessus_doc, checkfor, db)
-	
-	elif args.mode == "nipper":
-		print "The file to parse is Nessus"
-	elif args.mode == "john":
-		print "The file to parse is a John the Ripper Log File"
-		parse_john_log(args.inputfile, args.csvfile)
-	else:
-		print "Invalid File Type"
-
-
-	hrline = "-------------------------------------------------------------------------------------\n"
-
-	
-if __name__ ==  "__main__":
-	main()
